57e3a130a87e465871b73f5085a7af786c03f872
Akpemie Darlington Alloh <xbladex16@gmail.com>
2023-12-17 16:48:59 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,20 @@
-CREATE TABLE "Passengers" ("id", "passenger", "age", PRIMARY KEY("id"));
-CREATE TABLE "Airlines" ("airline", "concourses");
-CREATE TABLE "Flights" ("flight", "depart", "depart_time", "arrival", "arrival_time");
-CREATE TABLE "Check_ins" ("name_id", "name", "flight", "date", "time", FOREIGN KEY("name_id") REFERENCES "Passengers"("id"));
+CREATE TABLE "Passengers" ("id" INTEGER,
+                           "passenger" NOT NULL,
+                           "age" INTEGER,
+                           PRIMARY KEY("id"));
+
+CREATE TABLE "Airlines" ("airline" UNIQUE NOT NULL,
+                         "concourses" UNIQUE NOT NULL);
+
+CREATE TABLE "Flights" ("flight",
+                        "depart",
+                        "depart_time",
+                        "arrival",
+                        "arrival_time");
+
+CREATE TABLE "Check_ins" ("name_id",
+                          "name",
+                          "flight",
+                          "date",
+                          "time",
+                          FOREIGN KEY("name_id") REFERENCES "Passengers"("id"));
